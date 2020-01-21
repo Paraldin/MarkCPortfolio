9aad397beb0b0eb3c0f010085c75bb8f66a18825
Mark <markjcorum@gmail.com>
2020-01-21 09:02:56 -0500

    Update fizzBuzz.js

--- MarkCPortfolio/Template/js/fizzBuzz.js
+++ MarkCPortfolio/Template/js/fizzBuzz.js
@@ -7,16 +7,18 @@ function buzzIt(fizz, buzz) {
     if (fizz == "" && buzz == "") {
         buzzTron.innerHTML = '';
     } else {
+        let fizzBuzzResult = ``;
         for (let i = 1; i < 103; i++) {
-            if (i % fizz == 0 && i % buzz == 0) {
-                buzzTron.innerHTML = buzzTron.innerHTML + `<div class="bg-success col-md-2 col-xs-6 fizzbuzz">FizzBuzz</div>`;
+            if (!(i % fizz) && !(i % buzz)) {
+               fizzBuzzResult = fizzBuzzResult + `<div class="bg-success col-md-2 col-xs-6 fizzbuzz">FizzBuzz</div>`;
             } else if (i % fizz == 0) {
-                buzzTron.innerHTML = buzzTron.innerHTML + `<div class="bg-info col-md-2 col-xs-6 fizz">Fizz</div>`;
+               fizzBuzzResult = fizzBuzzResult + `<div class="bg-info col-md-2 col-xs-6 fizz">Fizz</div>`;
             } else if (i % buzz == 0) {
-                buzzTron.innerHTML = buzzTron.innerHTML + `<div class="bg-warning col-md-2 col-xs-6 buzz">Buzz</div>`;
+               fizzBuzzResult = fizzBuzzResult + `<div class="bg-warning col-md-2 col-xs-6 buzz">Buzz</div>`;
             } else {
-                buzzTron.innerHTML = buzzTron.innerHTML + `<div class="col-md-2 col-xs-6">${i}</div>`;
+               fizzBuzzResult = fizzBuzzResult + `<div class="col-md-2 col-xs-6">${i}</div>`;
             }
         }
+        buzzTron.innerHTML = fizzBuzzResult
     }
 }
